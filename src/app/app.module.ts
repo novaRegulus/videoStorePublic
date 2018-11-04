3c53b574ef79e313c06c6b8d7e1b17967900d2cc
novaRegulus <piromeroc@gmail.com>
2018-11-04 13:22:58 -0600

    se agrega el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { IndexComponent } from './index/index.component';
+import { SaleComponent } from './sale/sale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingUpComponent,
+    IndexComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule
